4016863488772bb4b1125fd5976c6c34de652881
ricardojosers <ricardojosers@gmail.com>
2017-01-21 18:38:16 -0200

    Suporte para persistência de dados - parte 1

---
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/TipoPessoa.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)TipoPessoa.java
+ *
+ * Copyright 2014-2014, Ricardo Rodrigues.
+ * @ricardojosers
+ * 
+ * Todos os direitos reservados.
+ */
+ 
+package com.algaworks.pedidovenda.model;
+
+
+public enum TipoPessoa {
+    
+    FISICA, JURIDICA
+    
+}
